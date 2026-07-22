875ac29f4baa9a43cfc4b2188b9cee10269f3f3b
Shruti Joshi <shrutijoshi1481995@gmail.com>
2026-07-22 22:09:03 +0530

    security: Update Google OAuth Client ID config

--- frontend/src/config/googleConfig.js
+++ frontend/src/config/googleConfig.js
@@ -1,2 +1,2 @@
 // Public Google OAuth Client ID (Safe to commit, visible in client browser code)
-export const GOOGLE_CLIENT_ID = '179378750306-4q53e7f3rlfh8p3ueod6blg4liu9jve4.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_ID = '179378750306-g2fva46cghpm4eh97al02u3v8i5b1kjs.apps.googleusercontent.com';
